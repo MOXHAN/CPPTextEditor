3ae5e514ae961310668db379f41a4db853151a6e
maxim.hansen@student.fh-kiel.de <QTKe#n4n>
2023-05-16 21:07:27 +0200

    Added CMake, MainWindow class file, main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by maximhansen on 16.05.2023.
+//
+
+#include <QApplication>
+#include <QWindow>
+
+
+#include "MainWindow.h"
+
+int main(int argc, char *argv[]) {
+
+    //Create Application
+    QApplication a(argc, argv);
+    //Create Window
+    MainWindow w;
+
+    //Design window
+    w.resize(550,550);
+    w.setToolTip("QWidget");
+
+    //Show window
+    w.show();
+
+    //Starts main loop
+    return a.exec();
+}
+
